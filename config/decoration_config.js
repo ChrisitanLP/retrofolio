b7d14268ed58c248cf801278e03fcc5837d4dffa
Wolfly <cl009189@gmail.com>
2025-06-30 12:24:24 -0500

    Se agrega el archivo encargado de la configuración de las decoraciones del entorno

---
+++ config/decoration_config.js
@@ -0,0 +1,9 @@
+// config/decorationConfig.js
+export const DECORATION_CONFIG = {
+  // Configuración base
+  base: {
+    factorColision: 0.9,
+    recorteSuperior: 40,
+    zonaProferidadSuperior: 0.4
+  },
+};
